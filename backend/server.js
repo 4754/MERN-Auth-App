8555ad6344b5f2f9f29c7fa89f81177dbfc65158
4754 <gaurav2k9ssk54@gmail.com>
2023-11-24 07:27:21 +0530

    to deploy 	modified:   backend/server.js 	modified:   frontend/.gitignore 	new file:   frontend/dist/assets/index-078876d9.js 	new file:   frontend/dist/assets/index-9ed9f223.css 	new file:   frontend/dist/index.html 	new file:   frontend/dist/vite.svg

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path'
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
@@ -18,7 +19,19 @@ app.use(express.urlencoded({extended: true}))
 // user routes
 app.use(cookieParser())
 app.use('/api/users', userRoutes) 
-app.get('/',(req,res)=> res.send("server is running"));
+
+if(process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+    
+    app.get('*', (req,res)=> res.sendFile(path.resolve(__dirname, 'frontend', 'dist','index.html')))
+} else {
+    app.get('/',(req,res)=> res.send("server is running"));
+}
+
+
+
+
 app.use(notFound);
 app.use(errorHandler)
 
